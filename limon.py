796ea31e3240bf83ba5e41aa190f5523a08af58d
aixa-source <aixa8868@gmail.com>
2025-12-17 07:44:27 -0600

    cree mi presentacion limon.py

---
+++ limon.py
@@ -0,0 +1,14 @@
+def main():
+    nombre_completo = input("Ingresa tu nombre completo: ")
+    domicilio = input("Ingresa tu domicilio: ")
+    color_favorito = input("Ingresa tu color favorito: ")
+    hobbie = input("Ingresa tu hobbie: ")
+
+    print("\n--- DATOS INGRESADOS ---")
+    print("Nombre completo:", nombre_completo)
+    print("Domicilio:", domicilio)
+    print("Color favorito:", color_favorito)
+    print("Hobbie:", hobbie)
+
+if __name__ == "__main__":
+    main()
