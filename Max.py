50e84b5ab828281a652e815af5506167dc6af233
egamanflores94-sudo <megamanflores94@gmail.com>
2025-12-17 07:41:53 -0600

    Cree mi presentacion

---
+++ Max.py
@@ -0,0 +1,14 @@
+def main():
+    nombre = input("Ingresa tu nombre completo: ")
+    CodigoPostal = input("Ingresa tu domicilio: ")
+    colorfavorito = input("Ingresa tu color favorito: ")
+    hobbie = input("Ingresa tu hobbie: ")
+
+    print("----- DATOS INGRESADOS ---------")
+    print("Nombre completo:", nombre)
+    print("Domicilio:", CodigoPostal)
+    print("Color favorito:", colorfavorito)
+    print("Hobbie:", hobbie)
+
+if __name__ == "__main__":
+    main()
